de77dcb18135ce84c0cfcf3912d159931c0a6dac
Mathimalar190505 <malarmathi826@gmail.com>
2026-02-17 19:07:36 +0530

    Revert "Merge pull request #4 from Mathimalar190505/dev"
    This reverts commit d14a1eafaf29256c888dd06fd5033069bf430523, reversing
    changes made to 80fbca9af222e8a967c23aa983a68d8147c765ef.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,70 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Root from "./utilis/Root";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoutes from "./utilis/ProtectedRoutes";
-import Categories from "./components/Categories";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter as Router,Routes,Route} from 'react-router'
+import Root from './utilis/Root'
+import Login from './pages/Login'
+import ProtectedRoutes from './utilis/ProtectedRoutes'
 
-function App() {
+ function App() {
   return (
     <Router>
       <Routes>
-
-        {/* Root redirect controller */}
-        <Route path="/" element={<Root />} />
-
-        {/* Public Routes */}
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/unauthorized"
-          element={
-            <p className="font-bold text-3xl mt-20 ml-20">
-              Unauthorized
-            </p>
-          }
-        />
-
-        {/* Admin Dashboard */}
-        <Route
-          path="/admin/dashboard"
-          element={
-            <ProtectedRoutes requireRole={["admin"]}>
-              <Dashboard />
-            </ProtectedRoutes>
-          }
-        >
-          <Route
-            index
-            element={<h1 className="text-3xl font-bold">Admin Summary</h1>}
-          />
-          <Route
-            path="categories"
-            element={<Categories/>}/>
-            <Route
-            path="products"
-            element={<h1 className="text-3xl font-bold" >products</h1>}
-            />
-            <Route
-            path="supplier"
-            element={<h1 className="text-3xl font-bold">supplier</h1>}/>
-            <Route
-            path="orders"
-            element={<h1 className="text-3xl font-bold">orders</h1>}/>
-            <Route
-            path="users"
-            element={<h1 className="text-3xl font-bold">Admin Users</h1>}/>
-            
-        </Route>
-
-        {/* Customer Dashboard */}
-        <Route
-          path="/customer/dashboard"
-          element={<h1>Customer dashboard</h1>}
-        />
-
+        <Route path="/" element={<Root />}/>
+        <Route path="/admin/dashboard" element={<ProtectedRoutes requireRole={["admin"]}><h1>admin dashboard</h1></ProtectedRoutes>}/>
+        <Route path="/customer/dashboard" element={<h1>Customer dashboard</h1>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/unauthorized" element={<p className='font-bold text-3xl mt-20 ml-20'>Unauthorized</p>}/>
       </Routes>
     </Router>
   );
 }
 
-export default App;
+
+export default App
--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
 import authRoutes from './routes/auth.js';
-import categoryRoutes from './routes/category.js';
 
 dotenv.config();
 
@@ -11,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth',authRoutes);
-app.use('/api/category',categoryRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("API running without MongoDB 🚀");
