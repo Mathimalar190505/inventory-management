f92078138c4dd6817039b5bbdb08136f0cd87500
Mathimalar <malarmathi826@gmail.com>
2026-04-14 09:30:00 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -13,14 +13,7 @@ import dashboardRoutes from './routes/dashboard.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://inventory-management-ruby-five.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/auth',authRoutes);
